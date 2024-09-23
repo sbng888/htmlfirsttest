6e1d19bcb538ff10b93d82c9899a0cc1b8f3e7e6
sbng888 <sbng888@gmail.com>
2024-09-23 18:38:17 +0800

    9

---
+++ JSS/student.js
@@ -0,0 +1,14 @@
+class Student {
+    constructor(name, age, height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    displayInfo(){
+        return `Name is ${this.name}, Age is ${this.age} , Height is ${this.height}`;
+    }
+
+    
+
+};
